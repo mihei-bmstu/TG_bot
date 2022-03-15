3cffee22ab6352fb37365bc07cf07af441b6ce03
mihei-bmstu <mihei-bmstu@mail.ru>
2022-03-15 16:58:45 +0300

    receive and re-send an image

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import telebot
 
+TOKEN = '5129375580:AAHrcxtW24Pa4zDfkAkByx-SAj-fK6jS9gw'
+bot = telebot.TeleBot(TOKEN)
 
-# Создаем экземпляр бота
-bot = telebot.TeleBot('5129375580:AAHrcxtW24Pa4zDfkAkByx-SAj-fK6jS9gw')
-# Функция, обрабатывающая команду /start
+
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+    """This function threats /start command"""
+    bot.send_message(m.chat.id, 'Please, send me a photo to identify')
 # Получение сообщений от юзера
-@bot.message_handler(content_types=["text"])
+
+
+@bot.message_handler(content_types=["text", 'photo'])
 def handle_text(message):
-    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
-# Запускаем бота
+    """Message handler"""
+    if message.content_type == 'photo':
+        raw = message.photo[2].file_id
+        name = raw + '.jpg'
+        file_info = bot.get_file(raw)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open(name, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        img = open(name, 'rb')
+        #bot.send_photo(message.chat.id, img)
+        bot.send_message(message.chat.id, 'Thank you!')
+
+
 bot.polling(none_stop=True, interval=0)
