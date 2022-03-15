844a6eb01411177fb2f2abb450f4b14fd0129557
mihei-bmstu <mihei-bmstu@mail.ru>
2022-03-15 17:43:32 +0300

    alpha-test version 1

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import telebot
+from network import load_model, classify_image
 
 TOKEN = '5129375580:AAHrcxtW24Pa4zDfkAkByx-SAj-fK6jS9gw'
 bot = telebot.TeleBot(TOKEN)
+model = load_model()
 
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
     """This function threats /start command"""
     bot.send_message(m.chat.id, 'Please, send me a photo to identify')
-# Получение сообщений от юзера
 
 
 @bot.message_handler(content_types=["text", 'photo'])
@@ -22,8 +23,13 @@ def handle_text(message):
         with open(name, 'wb') as new_file:
             new_file.write(downloaded_file)
         img = open(name, 'rb')
-        #bot.send_photo(message.chat.id, img)
-        bot.send_message(message.chat.id, 'Thank you!')
+        result = classify_image(name, model)
+        print(result)
+        print(type(result))
+        # bot.send_photo(message.chat.id, img)
+        text = '\n'.join(result)
+        print(text)
+        bot.send_message(message.chat.id, 'Thank you! \n Top 5 categories for your image: \n' + text)
 
 
 bot.polling(none_stop=True, interval=0)
--- network.py
+++ network.py
@@ -1,37 +1,42 @@
 import torch
 from torchvision import transforms
 from PIL import Image
-import matplotlib.pyplot as plt
 
-model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=True)
-#model = torch.load('alexnet-owt-7be5be79.pth')
-model.eval()
-filename = 'plane.jpeg'
-input_image = Image.open(filename)
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-input_tensor = preprocess(input_image)
-input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
 
-# move the input and model to GPU for speed if available
-if torch.cuda.is_available():
-    input_batch = input_batch.to('cuda')
-    model.to('cuda')
+def load_model():
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=True)
+    model.eval()
+    return model
 
-with torch.no_grad():
-    output = model(input_batch)
 
-# The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-probabilities = torch.nn.functional.softmax(output[0], dim=0)
+def classify_image(filename, model):
+    input_image = Image.open(filename)
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
 
-# Read the categories
-with open("imagenet_classes.txt", "r") as f:
-    categories = [s.strip() for s in f.readlines()]
-# Show top categories per image
-top5_prob, top5_catid = torch.topk(probabilities, 3)
-for i in range(top5_prob.size(0)):
-    print(categories[top5_catid[i]], top5_prob[i].item())
+    if torch.cuda.is_available():
+        input_batch = input_batch.to('cuda')
+        model.to('cuda')
+
+    with torch.no_grad():
+        output = model(input_batch)
+
+    # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+
+    # Read the categories
+    with open("imagenet_classes.txt", "r") as f:
+        categories = [s.strip() for s in f.readlines()]
+    # Show top categories per image
+    top5_prob, top5_catid = torch.topk(probabilities, 5)
+    list_cat = []
+    for i in range(top5_prob.size(0)):
+        list_cat.append(categories[top5_catid[i]])
+        list_cat.append(str(top5_prob[i].item() * 100) + '%')
+    return list_cat
